861146ffb77b210875ac26326332c67a283d8820
BC371 <61798546+BC371@users.noreply.github.com>
2021-02-16 17:25:28 +0800

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿
+namespace ImageSeer
+{
+	partial class Form1
+	{
+		/// <summary>
+		///  Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		///  Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		///  Required method for Designer support - do not modify
+		///  the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+			this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Default;
+			this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(182, 153);
+			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.pictureBox1.TabIndex = 0;
+			this.pictureBox1.TabStop = false;
+			this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+			// 
+			// Form1
+			// 
+			this.AllowDrop = true;
+			this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.AutoValidate = System.Windows.Forms.AutoValidate.Disable;
+			this.ClientSize = new System.Drawing.Size(182, 153);
+			this.ControlBox = false;
+			this.Controls.Add(this.pictureBox1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+			this.MaximizeBox = false;
+			this.MinimizeBox = false;
+			this.Name = "Form1";
+			this.TopMost = true;
+			this.SizeChanged += new System.EventHandler(this.Form1_SizeChanged);
+			this.DragDrop += new System.Windows.Forms.DragEventHandler(this.Form1_DragDrop);
+			this.DragEnter += new System.Windows.Forms.DragEventHandler(this.Form1_DragEnter);
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.PictureBox pictureBox1;
+	}
+}
+
---
+++ Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace ImageSeer
+{
+	public partial class Form1 : Form
+	{
+		Point p;
+		int all_pm = 1;
+		double origin_rate = 1.0;
+		int w = 200;
+		//Console.WriteLine("hello");
+		[DllImport("user32.dll")]
+		public static extern bool ReleaseCapture();
+		[DllImport("user32.dll")]
+		public static extern bool SendMessage(IntPtr hwnd, int wMsg, int wParam, int lParam);
+		public const int WM_SYSCOMMAND = 0x0112;
+
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private int abs(int ab)
+		{
+			all_pm = (2 * ab + 1) % 2;
+			return all_pm * ab;
+
+		}
+
+		private void Form1_SizeChanged(object sender, EventArgs e)
+		{
+			if (this.Width != w)
+			{
+
+				this.Height = (int)(this.Width / origin_rate);
+			}
+			else
+			{
+				this.Width = (int)(this.Height * origin_rate);
+
+			}
+			w = this.Width;
+		}
+
+		private void Form1_DragDrop(object sender, DragEventArgs e)
+		{
+			pictureBox1.Image = new Bitmap(((Array)e.Data.GetData(DataFormats.FileDrop)).GetValue(0).ToString());
+			this.Width = pictureBox1.Image.Width;
+			this.Height = pictureBox1.Image.Height;
+			w = this.Width;
+			origin_rate = pictureBox1.Image.Width / (float)pictureBox1.Image.Height;
+
+		}
+
+		private void Form1_DragEnter(object sender, DragEventArgs e)
+		{
+			if (e.Data.GetDataPresent(DataFormats.FileDrop))
+				e.Effect = DragDropEffects.Link;
+			else e.Effect = DragDropEffects.None;
+
+		}
+
+		private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+		{
+			ReleaseCapture();
+			if (this.Width - abs(2 * e.X - this.Width) < 40)
+			{
+				this.Cursor = Cursors.SizeWE;
+				SendMessage(this.Handle, WM_SYSCOMMAND, 0xF001 + (all_pm + 1) / 2 * 0x0001, 0);
+
+
+			}
+			else if (this.Height - abs(2 * e.Y - this.Height) < 40)
+			{
+				this.Cursor = Cursors.SizeNS;
+				SendMessage(this.Handle, WM_SYSCOMMAND, 0xF003 + (all_pm + 1) / 2 * 0x0003, 0);
+			}
+			else
+			{
+				this.Cursor = Cursors.SizeAll;
+				SendMessage(this.Handle, WM_SYSCOMMAND, 0xF012, 0);
+
+			}
+
+		}
+
+	}
+}
